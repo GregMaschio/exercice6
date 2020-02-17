939910f952481ac2311a832c0c9dbb7a1dbef4a7
Grégoire Maschio <gregmaschio@hotmail.com>
2020-02-17 10:54:24 -0500

    ajout de la plupart des fonctionnalités dont logo images, liens, etc.

--- functions.php
+++ functions.php
@@ -4,3 +4,10 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  
 }
+
+function add_google_fonts(){
+    wp_enqueue_style('google_web_fonts','https://fonts.googleapis.com/css?family=Roboto&display=swap');
+}
+
+add_filter('show_admin_bar', '__return_false');
+add_action('wp_enqueue_scripts','add_google_fonts');
